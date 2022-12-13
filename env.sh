fd50033f1d2d2c3ae1b123e2303e6c7ab37e959d
Eric MacKay <69176954+ericmackay5@users.noreply.github.com>
2022-12-13 16:07:56 -0500

    Update env.sh

--- env.sh
+++ env.sh
@@ -2,9 +2,11 @@
 export partitions=6
 
 # important for data durability. Advised to set to 3.
-export replication.factor=3
+export replication_factor=3
 
 # topic configuration that will affect acks parameter from producer config. When acks=all we can tolerate (#replication.factor) - (#min.insync.replicas) brokers going down
-export min.insync.replicas=2
+export min_insync_replicas=2
 
-export BOOTSTRAP_SERVER=pkc-n00kk.us-east-1.aws.confluent.cloud:9092
+export BOOTSTRAP_SERVER="<<BOOTSRATP_SERVER>>"
+export CLUSTER_API_KEY='<<CLUSTER_API_KEY>>'
+export CLUSTER_SECRET_KEY='<<CLUSTER_SECRET_KEY>>'
